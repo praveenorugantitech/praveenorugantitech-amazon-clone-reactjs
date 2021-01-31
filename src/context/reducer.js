1ea5db931fa660d6078a0a9068b7d4f1b843866d
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-31 14:11:27 +0530

    removed console logs

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -8,7 +8,7 @@ export const getBasketTotal = (basket) =>
   basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
-  console.log(action);
+
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
@@ -31,10 +31,6 @@ const reducer = (state, action) => {
       if (index >= 0) {
         newBasket.splice(index, 1);
 
-      } else {
-        console.warn(
-          `Cant remove product (id: ${action.id}) as its not in basket!`
-        )
       }
 
       return {
