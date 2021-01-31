b17a7309a2d13291b6a2357aef7d8f0851da6fb0
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-31 13:11:15 +0530

    updated firebase details

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from "../firebase";
+import { db } from "../config/firebase";
 import '../css/Orders.css'
 import { useStateValue } from "../context/StateProvider";
 import Order from './Order'
